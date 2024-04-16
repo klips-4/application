80ef31cdc7fe1f1b1d0bad93b1b419af9373db28
klips-4 <95581718+klips-4@users.noreply.github.com>
2024-04-16 17:27:46 +0500

    test

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ class TopLabel(ctk.CTkFrame):
         self.drop_down()
         self.grid_columnconfigure((0, 0), weight=1)
         self.product_from_db = []
+        self.volume_from_db = []
+
 
     def select_object(self, selected_object):
         self.product_drop_down.configure(state="normal")
@@ -35,42 +37,53 @@ class TopLabel(ctk.CTkFrame):
 
         return self.product_from_db
 
-    def select_product(self, selected_object):
-        return self.product_from_db
+    def select_product(self, selected_product):
+        self.volume_drop_down.configure(state="normal")
+        self.volume_from_db = engine.choose_volume(selected_product)
+        self.volume_drop_down.configure(values=self.volume_from_db)
+
+
 
     def init_top_widget(self):
         self.top_label = ctk.CTkFrame(self, fg_color="aliceblue")
         self.top_label.grid(padx=10, pady=(10, 10), sticky="nsew")
 
     def drop_down(self):
+        # Objects
+
         self.choose_object = ctk.CTkLabel(self.top_label, text="Выбрать объект:")
         self.choose_object.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
 
         self.objects = object_from_db
 
-        self.object_drop_down = ctk.CTkComboBox(self.top_label, values=self.objects, command=self.select_object,
-                                                )
+        self.object_drop_down = ctk.CTkComboBox(self.top_label, values=self.objects, command=self.select_object,)
         self.object_drop_down.grid(row=1, column=0, padx=10, pady=(10, 10), sticky="nsew")
+
+        # Products
 
         self.choose_product = ctk.CTkLabel(self.top_label, text="Выбрать продукт:")
         self.choose_product.grid(row=0, column=1, padx=50, pady=(10, 0), sticky="nsew")
 
-        # self.products = self.product_from_db
-
-        self.product_drop_down = ctk.CTkComboBox(self.top_label, state='disabled')
+        self.product_drop_down = ctk.CTkComboBox(self.top_label, state='disabled', command=self.select_product)
         self.product_drop_down.grid(row=1, column=1, padx=10, pady=(10, 10), sticky="nsew")
+
+        # Volume
 
         self.choose_value = ctk.CTkLabel(self.top_label, text="Диаметр труб-да/объем резервуара:")
         self.choose_value.grid(row=0, column=2, padx=10, pady=(10, 0), sticky="nsew")
 
-        self.value_drop_down = ctk.CTkComboBox(self.top_label, state='disabled')
-        self.value_drop_down.grid(row=1, column=2, padx=10, pady=(10, 10), sticky="nsew")
+        self.volume_drop_down = ctk.CTkComboBox(self.top_label, state='disabled')
+        self.volume_drop_down.grid(row=1, column=2, padx=10, pady=(10, 10), sticky="nsew")
+
+        # Construction
 
         self.choose_construction = ctk.CTkLabel(self.top_label, text="Способ строительства:")
         self.choose_construction.grid(row=0, column=3, padx=10, pady=(10, 0), sticky="nsew")
 
         self.construction_drop_down = ctk.CTkComboBox(self.top_label, state='disabled')
         self.construction_drop_down.grid(row=1, column=3, padx=10, pady=(10, 10), sticky="nsew")
+
+        # Life_cycle
 
         self.choose_life_cycle = ctk.CTkLabel(self.top_label, text="Этап жизненного цикла:")
         self.choose_life_cycle.grid(row=0, column=4, padx=10, pady=(10, 0), sticky="nsew")
@@ -91,9 +104,9 @@ class BottomLabel(ctk.CTkFrame):
         self.bottom_label.grid(column=0, row=1, padx=10, pady=(10, 10), sticky="nsew")
 
 
-engine = Connection()
-object_from_db = engine.choose_object()
+# engine = Connection()
+# object_from_db = engine.choose_object()
 
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+# if __name__ == "__main__":
+#     app = App()
+#     app.mainloop()
--- database/connection.py
+++ database/connection.py
@@ -1,15 +1,61 @@
 import sqlite3
 
 
-class Connection:
-    res = []
+# class Connection:
+#     res = []
+#
+#     def __init__(self):
+#         self.uniq_obj = []
+#         self.uniq_prod = []
+#         self.uniq_vol = []
+#         self.connection_to_db()
+#         self.choose_object()
+#
+#     def connection_to_db(self):
+#         with sqlite3.connect('database/database.db') as db:
+#             cursor = db.cursor()
+#             query = "SELECT * FROM Лист1"
+#             cursor.execute(query)
+#             self.res = cursor.fetchall()
+#
+#         return self.res
+#
+#     def choose_object(self):
+#         for objects in self.res:
+#             if objects[2] not in self.uniq_obj:
+#                 self.uniq_obj.append(objects[2])
+#         return self.uniq_obj
+#
+#     def choose_product(self, type_object):
+#         for product in self.res:
+#             if product[4] not in self.uniq_prod:
+#                 self.uniq_prod.append(product[4])
+#         if type_object == "Резервуар":
+#             return [self.uniq_prod[0], self.uniq_prod[1]]
+#         elif type_object == "Трубопровод":
+#             return [self.uniq_prod[1], self.uniq_prod[3]]
+#
+#     def choose_volume(self, type_object):
+#         for vol in self.res:
+#             if vol[3] not in self.uniq_vol:
+#                 self.uniq_vol.append(vol[3])
+#         if type_object == "Нефть":
+#
+#             return [self.uniq_vol[0], self.uniq_vol[1], self.uniq_vol[2]]
+#         elif type_object == "Нефтепродукт":
+#             return [self.uniq_vol[3], self.uniq_vol[4], self.uniq_vol[5], self.uniq_vol[6], self.uniq_vol[7],
+#                     self.uniq_vol[8], self.uniq_vol[9]]
 
+
+class Connection:
+    def connection_to_db(self):
+        return
+
+
+class ConnWithObj(Connection):
     def __init__(self):
-        self.uniq_obj = []
-        self.uniq_prod = []
-        self.connection_to_db()
-        self.choose_object()
-        # self.choose_product()
+        Connection.__init__(self)
+        self.res = []
 
     def connection_to_db(self):
         with sqlite3.connect('database/database.db') as db:
@@ -19,18 +65,3 @@ class Connection:
             self.res = cursor.fetchall()
 
         return self.res
-
-    def choose_object(self):
-        for objects in self.res:
-            if objects[2] not in self.uniq_obj:
-                self.uniq_obj.append(objects[2])
-        return self.uniq_obj
-
-    def choose_product(self, object):
-        for product in self.res:
-            if product[4] not in self.uniq_prod:
-                self.uniq_prod.append(product[4])
-        if object == "Резервуар":
-            return [self.uniq_prod[0], self.uniq_prod[1]]
-        elif object == "Трубопровод":
-            return [self.uniq_prod[1], self.uniq_prod[3]]
