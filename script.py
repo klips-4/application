b39db38125a035d0f205d0ab5895db8458fe2ebd
klips-4 <klips-4@yandex.ru>
2024-04-18 23:03:07 +0500

    18/04/24 - 1

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import sqlite3
 
-# Конвертирование xlsx в db
-# if __name__ == "__main__":
-#     df = pd.read_excel('./database/denis_db.xlsx')
-#
-#     conn = sqlite3.connect('database/dataset.db')
-#     df.to_sql('dataset', conn, if_exists='replace', index=False)
-#     conn.close()
-#
-#
+
+if __name__ == "__main__":
+    df = pd.read_excel('./database/denis_db.xlsx')
+
+    conn = sqlite3.connect('database/dataset.db')
+    df.to_sql('dataset', conn, if_exists='replace', index=False)
+    conn.close()
+
+
